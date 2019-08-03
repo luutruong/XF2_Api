7bda66c240d6785ee66585fb84e77723715f588e
Luu Truong <truonglv@outlook.com>
2019-08-03 11:58:21 +0700

    Support to prune inactive devices

--- Cron/Auto.php
+++ Cron/Auto.php
@@ -2,6 +2,8 @@
 
 namespace Truonglv\Api\Cron;
 
+use XF\Timer;
+
 class Auto
 {
     public static function runHourly()
@@ -9,6 +11,24 @@ class Auto
         $logLength = \XF::options()->tApi_logLength;
         if ($logLength > 0) {
             \XF::db()->delete('xf_tapi_log', 'log_date <= ?', \XF::$time - $logLength * 86400);
+        }
+
+        $subscriptionInactiveLength = \XF::options()->tApi_inactiveDeviceLength;
+        if ($subscriptionInactiveLength > 0) {
+            $timer = new Timer(3);
+            $entities = \XF::finder('Truonglv\Api:Subscription')
+                ->where('subscribed_date', '<=', \XF::$time - $subscriptionInactiveLength * 86400)
+                ->order('subscribed_date')
+                ->limit(20)
+                ->fetch();
+
+            foreach ($entities as $entity) {
+                $entity->delete(false);
+
+                if ($timer->limitExceeded()) {
+                    break;
+                }
+            }
         }
     }
 
