5a63a4fdadc4a6c3bf8f744f754c0b7c97008bc8
Luu Truong <truonglv@outlook.com>
2020-03-24 16:04:34 +0700

    Support response about data

--- XF/Entity/User.php
+++ XF/Entity/User.php
@@ -30,5 +30,66 @@ class User extends XFCP_User
         // if this key is TRUE in the user profile will have a tick icon
         // make this option config from server to support third party add-on
         $result->tapi_is_verified = $this->is_staff;
+
+        if ($verbosity >= self::VERBOSITY_VERBOSE) {
+            $result->tapi_about_data = $this->getTApiAboutTabData();
+        }
+    }
+
+    /**
+     * @return array
+     */
+    protected function getTApiAboutTabData()
+    {
+        $data = [];
+
+        if ($this->custom_title !== '') {
+            $data[] = [
+                'label' => \XF::phrase('custom_title'),
+                'value' => $this->custom_title,
+            ];
+        }
+
+        $birthday = $this->Profile->getBirthday();
+        if (\count($birthday) > 0) {
+            $language = $this->app()->language(\XF::visitor()->language_id);
+
+            $data[] = [
+                'label' => \XF::phrase('date_of_birth'),
+                'value' => $language->date($birthday['timeStamp'], $birthday['format']),
+            ];
+        }
+
+        if ($this->Profile->website !== '') {
+            $data[] = [
+                'label' => \XF::phrase('website'),
+                'value' => $this->Profile->website,
+            ];
+        }
+
+        if ($this->Profile->location !== '') {
+            $data[] = [
+                'label' => \XF::phrase('location'),
+                'value' => $this->Profile->location,
+            ];
+        }
+
+        $showingCustomFieldIds = ['skype', 'facebook', 'twitter'];
+        $customFields = $this->Profile->custom_fields;
+
+        foreach ($showingCustomFieldIds as $customFieldId) {
+            $value = $customFields->getFieldValue($customFieldId);
+            if ($value === null) {
+                continue;
+            }
+
+            $definition = $customFields->getDefinition($customFieldId);
+            $data[] = [
+                'label' => $definition->title,
+                'value' => $value,
+            ];
+        }
+
+        return $data;
     }
 }
