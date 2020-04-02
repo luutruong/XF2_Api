5dd268f563dba10499970beff96224488be92e04
Luu Truong <truonglv@outlook.com>
2020-04-03 06:29:36 +0700

    Suppor quote conversation message

--- XF/Api/Controller/ConversationMessages.php
+++ XF/Api/Controller/ConversationMessages.php
@@ -13,4 +13,35 @@ class ConversationMessages extends XFCP_ConversationMessages
 
         return parent::actionPost($params);
     }
+
+    /**
+     * @param \XF\Entity\ConversationMaster $conversation
+     * @return \XF\Service\Conversation\Replier
+     */
+    protected function setupConversationReply(\XF\Entity\ConversationMaster $conversation)
+    {
+        $replier = parent::setupConversationReply($conversation);
+
+        if (App::isRequestFromApp()) {
+            $quoteMessageId = $this->filter('quote_message_id', 'uint');
+            $defaultMessage = null;
+            if ($quoteMessageId > 0) {
+                /** @var \XF\Entity\ConversationMessage|null $message */
+                $message = $this->em()->find('XF:ConversationMessage', $quoteMessageId, 'User');
+                if ($message && $message->conversation_id == $conversation->conversation_id) {
+                    $defaultMessage = $message->getQuoteWrapper(
+                        $this->app->stringFormatter()->getBbCodeForQuote($message->message, 'conversation_message')
+                    );
+                }
+            }
+
+            $message = $this->filter('message', 'str');
+            if ($defaultMessage !== null) {
+                $message = $defaultMessage . "\n" . $message;
+                $replier->setMessageContent($message);
+            }
+        }
+
+        return $replier;
+    }
 }
