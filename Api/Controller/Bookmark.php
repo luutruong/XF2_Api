1e769ddbc550a32a81a1c90ccdb97a37594b29a6
Luu Truong <truonglv@outlook.com>
2021-03-22 07:16:20 +0700

    Update Bookmark.php

--- Api/Controller/Bookmark.php
+++ Api/Controller/Bookmark.php
@@ -3,9 +3,51 @@
 namespace Truonglv\Api\Api\Controller;
 
 use XF\Api\Controller\AbstractController;
+use XF\Mvc\Entity\Entity;
+use ZixVN\Exams\App;
 
 class Bookmark extends AbstractController
 {
+    public function actionGet()
+    {
+        $this->assertRequiredApiInput(['content_type']);
+        $this->assertRegisteredUser();
+
+        $contentType = $this->filter('content_type', 'str');
+
+        $visitor = \XF::visitor();
+        $finder = $this->finder('XF:BookmarkItem');
+
+        $finder->where('user_id', $visitor->user_id);
+        $finder->where('content_type', $contentType);
+        $finder->order('bookmark_date', 'DESC');
+
+        $page = $this->filterPage();
+        $perPage = 20;
+
+        $total = $finder->total();
+        $this->assertValidApiPage($page, $perPage, $total);
+
+        $results = $finder->limitByPage($page, $perPage)->fetchColumns('content_id');
+        $contentIds = array_column($results, 'content_id');
+
+        $contentFinder = $this->finder($this->app()->getContentTypeEntity($contentType));
+        $contentFinder->with('api');
+        $contentFinder->whereIds($contentIds);
+
+        $entities = $contentFinder->fetch();
+        if (\XF::isApiCheckingPermissions()) {
+            $entities = $entities->filterViewable();
+        }
+
+        $entities = $entities->sortByList($contentIds);
+
+        return $this->apiResult([
+            'entities' => $entities->toApiResults(Entity::VERBOSITY_VERBOSE),
+            'pagination' => $this->getPaginationData($contentIds, $page, $perPage, $total),
+        ]);
+    }
+
     public function actionPost()
     {
         $this->assertRequiredApiInput(['content_type', 'content_id']);
