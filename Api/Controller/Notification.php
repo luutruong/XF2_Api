8641da6abdac9f549bc3287746b20d800fa8df85
Luu Truong <truonglv@outlook.com>
2021-04-28 16:07:25 +0700

    Update notification api

--- Api/Controller/Notification.php
+++ Api/Controller/Notification.php
@@ -12,28 +12,55 @@ class Notification extends AbstractController
     public function actionGet(ParameterBag $params)
     {
         $this->assertRegisteredUser();
+        $alert = $this->assertViewableAlert($params['alert_id']);
 
-        /** @var UserAlert|null $alert */
-        $alert = $this->finder('XF:UserAlert')->whereId($params->alert_id)->fetchOne();
-        if ($alert === null || $alert->alerted_user_id !== \XF::visitor()->user_id) {
+        if (!\in_array($alert->content_type, \Truonglv\Api\App::getSupportAlertContentTypes(), true)) {
             return $this->notFound();
         }
 
-        $content = $alert->getContent();
-        if ($content === null
-            || !\in_array($alert->content_type, \Truonglv\Api\App::getSupportAlertContentTypes(), true)
-        ) {
-            return $this->notFound();
+        $handler = $alert->getHandler();
+        if ($handler === null) {
+            return $this->noPermission();
         }
+
+        /** @var Entity|null $content */
+        $content = $this->app()->findByContentType(
+            $alert->content_type,
+            $alert->content_id,
+            $handler->getEntityWith()
+        );
+
+        return $this->apiResult([
+            'notification' => $alert->toApiResult(Entity::VERBOSITY_VERBOSE),
+            'content' => $content === null ? null : $content->toApiResult(Entity::VERBOSITY_VERBOSE)
+        ]);
+    }
+
+    public function actionPostMarkRead(ParameterBag $params)
+    {
+        $alert = $this->assertViewableAlert($params['alert_id']);
 
         if ($alert->view_date <= 0) {
             $alert->view_date = \XF::$time;
             $alert->save();
         }
 
-        return $this->apiResult([
-            'notification' => $alert->toApiResult(Entity::VERBOSITY_VERBOSE),
-            'content' => $content->toApiResult()
-        ]);
+        return $this->apiSuccess();
+    }
+
+    /**
+     * @param mixed $alertId
+     * @return UserAlert
+     * @throws \XF\Mvc\Reply\Exception
+     */
+    protected function assertViewableAlert($alertId): UserAlert
+    {
+        /** @var UserAlert $alert */
+        $alert = $this->assertRecordExists('XF:UserAlert', $alertId);
+        if ($alert->alerted_user_id !== \XF::visitor()->user_id) {
+            throw $this->exception($this->noPermission());
+        }
+
+        return $alert;
     }
 }
