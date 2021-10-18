d20f52234478858bad6abef7f9544b50568d5333
Luu Truong <truonglv@outlook.com>
2021-10-19 06:54:39 +0700

    Fix quote convo message

--- XF/Api/Controller/ConversationMessages.php
+++ XF/Api/Controller/ConversationMessages.php
@@ -12,29 +12,27 @@ class ConversationMessages extends XFCP_ConversationMessages
     {
         $replier = parent::setupConversationReply($conversation);
 
-        if ($this->request()->exists('quote_message_id')) {
-            $quoteMessageId = $this->filter('quote_message_id', 'uint');
-            $defaultMessage = null;
-            if ($quoteMessageId > 0) {
-                /** @var \XF\Entity\ConversationMessage|null $message */
-                $message = $this->em()->find('XF:ConversationMessage', $quoteMessageId, 'User');
-                if ($message !== null && $message->conversation_id == $conversation->conversation_id) {
-                    $defaultMessage = $message->getQuoteWrapper(
-                        $this->app->stringFormatter()->getBbCodeForQuote($message->message, 'conversation_message')
-                    );
-                }
+        $quoteMessageId = $this->filter('quote_message_id', 'uint');
+        $defaultMessage = null;
+        if ($quoteMessageId > 0) {
+            /** @var \XF\Entity\ConversationMessage|null $message */
+            $message = $this->em()->find('XF:ConversationMessage', $quoteMessageId, 'User');
+            if ($message !== null && $message->conversation_id == $conversation->conversation_id) {
+                $defaultMessage = $message->getQuoteWrapper(
+                    $this->app->stringFormatter()->getBbCodeForQuote($message->message, 'conversation_message')
+                );
             }
-
-            $message = $this->filter('message', 'str');
-            /** @var \Truonglv\Api\Api\ControllerPlugin\Quote $quotePlugin */
-            $quotePlugin = $this->plugin('Truonglv\Api:Api:Quote');
-            $message = $quotePlugin->prepareMessage($message, 'conversation_message');
-
-            if ($defaultMessage !== null) {
-                $message = $defaultMessage . "\n" . $message;
-            }
-            $replier->setMessageContent($message);
         }
+
+        $message = $this->filter('message', 'str');
+        /** @var \Truonglv\Api\Api\ControllerPlugin\Quote $quotePlugin */
+        $quotePlugin = $this->plugin('Truonglv\Api:Api:Quote');
+        $message = $quotePlugin->prepareMessage($message, 'conversation_message');
+
+        if ($defaultMessage !== null) {
+            $message = $defaultMessage . "\n" . $message;
+        }
+        $replier->setMessageContent($message);
 
         return $replier;
     }
