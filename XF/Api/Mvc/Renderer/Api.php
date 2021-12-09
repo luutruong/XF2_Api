e2763f482669aad7ae04e036aaa0524520531ef7
Luu Truong <truonglv@outlook.com>
2021-12-09 20:03:49 +0700

    Remove check in-app

--- XF/Api/Mvc/Renderer/Api.php
+++ XF/Api/Mvc/Renderer/Api.php
@@ -13,20 +13,16 @@ class Api extends XFCP_Api
     protected function getVisitorResponseExtras()
     {
         $extra = parent::getVisitorResponseExtras();
+        $visitor = \XF::visitor();
 
-        $request = \XF::app()->request();
-        if (App::isRequestFromApp($request)) {
-            $visitor = \XF::visitor();
+        /** @var UserAlert $alertRepo */
+        $alertRepo = \XF::app()->repository('XF:UserAlert');
+        $finder = $alertRepo->findAlertsForUser($visitor->user_id);
+        $finder->where('view_date', 0);
+        $finder->where('content_type', App::getSupportAlertContentTypes());
 
-            /** @var UserAlert $alertRepo */
-            $alertRepo = \XF::app()->repository('XF:UserAlert');
-            $finder = $alertRepo->findAlertsForUser($visitor->user_id);
-            $finder->where('view_date', 0);
-            $finder->where('content_type', App::getSupportAlertContentTypes());
-
-            $extra['alerts_unread'] = $finder->total();
-            $extra['_alerts_unread'] = $visitor->alerts_unread;
-        }
+        $extra['alerts_unread'] = $finder->total();
+        $extra['_alerts_unread'] = $visitor->alerts_unread;
 
         return $extra;
     }
