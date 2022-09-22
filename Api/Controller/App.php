36bee81484a243aeb98e23c78268d853dabcddeb
Truong Luu <truonglv@outlook.com>
2022-09-22 16:31:58 +0700

    Support third-party.

--- Api/Controller/App.php
+++ Api/Controller/App.php
@@ -668,58 +668,10 @@ class App extends AbstractController
      */
     protected function getAppInfo(): array
     {
-        /** @var Reaction $reactionData */
-        $reactionData = $this->data('Truonglv\Api:Reaction');
-        $reactions = $reactionData->getReactions();
+        /** @var \Truonglv\Api\Api\ControllerPlugin\App $appPlugin */
+        $appPlugin = $this->plugin('Truonglv\Api:Api:App');
 
-        /** @var AddOn $addOnRepo */
-        $addOnRepo = $this->repository('XF:AddOn');
-        $addOns = $addOnRepo->getInstalledAddOnData();
-
-        /** @var Attachment $attachmentRepo */
-        $attachmentRepo = $this->repository('XF:Attachment');
-        $constraints = $attachmentRepo->getDefaultAttachmentConstraints();
-
-        $registrationSetup = $this->app()->options()->registrationSetup;
-
-        $privacyPolicyUrl = $this->app()->options()->privacyPolicyUrl;
-        $tosUrl = $this->app()->options()->tosUrl;
-
-        $info = [
-            'reactions' => $reactions,
-            'apiVersion' => $addOns['Truonglv/Api']['version_id'],
-            'allowRegistration' => (bool) $this->options()->registrationSetup['enabled'],
-            'defaultReactionId' => \Truonglv\Api\Option\Reaction::DEFAULT_REACTION_ID,
-            'defaultReactionText' => $reactions[\Truonglv\Api\Option\Reaction::DEFAULT_REACTION_ID]['text'],
-            'quotePlaceholderTemplate' => \Truonglv\Api\App::QUOTE_PLACEHOLDER_TEMPLATE,
-            'allowedAttachmentExtensions' => $constraints['extensions'],
-            'registerMinimumAge' => $registrationSetup['requireDob'] > 0
-                ? intval($registrationSetup['minimumAge'])
-                : 0,
-            'connectedAccountProviders' => [
-                'apple' => $this->options()->tApi_caAppleProviderId,
-            ],
-            'appName' => $this->options()->tApi_appName,
-            'xfrmEnabled' => \Truonglv\Api\App::canViewResources(),
-        ];
-
-        if ($privacyPolicyUrl['type'] === 'custom') {
-            $info['privacyPolicyUrl'] = $privacyPolicyUrl['custom'];
-        } elseif ($privacyPolicyUrl['type'] === 'default') {
-            $info['privacyPolicyUrl'] = $this->app()->router('public')->buildLink('canonical:help/privacy-policy');
-        } else {
-            $info['privacyPolicyUrl'] = '';
-        }
-
-        if ($tosUrl['type'] === 'custom') {
-            $info['tosUrl'] =  $tosUrl['custom'];
-        } elseif ($tosUrl['type'] === 'default') {
-            $info['tosUrl'] = $this->app()->router('public')->buildLink('canonical:help/terms');
-        } else {
-            $info['tosUrl'] = '';
-        }
-
-        return $info;
+        return $appPlugin->getAppInfo();
     }
 
     protected function getNewsFeedsFilters(): array
---
+++ Api/ControllerPlugin/App.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Truonglv\Api\Api\ControllerPlugin;
+
+use Truonglv\Api\Data\Reaction;
+use XF\Api\ControllerPlugin\AbstractPlugin;
+use XF\Repository\AddOn;
+use XF\Repository\Attachment;
+
+class App extends AbstractPlugin
+{
+    /**
+     * @return array
+     */
+    public function getAppInfo(): array
+    {
+        /** @var Reaction $reactionData */
+        $reactionData = $this->data('Truonglv\Api:Reaction');
+        $reactions = $reactionData->getReactions();
+
+        /** @var AddOn $addOnRepo */
+        $addOnRepo = $this->repository('XF:AddOn');
+        $addOns = $addOnRepo->getInstalledAddOnData();
+
+        /** @var Attachment $attachmentRepo */
+        $attachmentRepo = $this->repository('XF:Attachment');
+        $constraints = $attachmentRepo->getDefaultAttachmentConstraints();
+
+        $registrationSetup = $this->app()->options()->registrationSetup;
+
+        $privacyPolicyUrl = $this->app()->options()->privacyPolicyUrl;
+        $tosUrl = $this->app()->options()->tosUrl;
+
+        $info = [
+            'reactions' => $reactions,
+            'apiVersion' => $addOns['Truonglv/Api']['version_id'],
+            'allowRegistration' => (bool) $this->options()->registrationSetup['enabled'],
+            'defaultReactionId' => \Truonglv\Api\Option\Reaction::DEFAULT_REACTION_ID,
+            'defaultReactionText' => $reactions[\Truonglv\Api\Option\Reaction::DEFAULT_REACTION_ID]['text'],
+            'quotePlaceholderTemplate' => \Truonglv\Api\App::QUOTE_PLACEHOLDER_TEMPLATE,
+            'allowedAttachmentExtensions' => $constraints['extensions'],
+            'registerMinimumAge' => $registrationSetup['requireDob'] > 0
+                ? intval($registrationSetup['minimumAge'])
+                : 0,
+            'connectedAccountProviders' => [
+                'apple' => $this->options()->tApi_caAppleProviderId,
+            ],
+            'appName' => $this->options()->tApi_appName,
+            'xfrmEnabled' => \Truonglv\Api\App::canViewResources(),
+        ];
+
+        if ($privacyPolicyUrl['type'] === 'custom') {
+            $info['privacyPolicyUrl'] = $privacyPolicyUrl['custom'];
+        } elseif ($privacyPolicyUrl['type'] === 'default') {
+            $info['privacyPolicyUrl'] = $this->app()->router('public')->buildLink('canonical:help/privacy-policy');
+        } else {
+            $info['privacyPolicyUrl'] = '';
+        }
+
+        if ($tosUrl['type'] === 'custom') {
+            $info['tosUrl'] =  $tosUrl['custom'];
+        } elseif ($tosUrl['type'] === 'default') {
+            $info['tosUrl'] = $this->app()->router('public')->buildLink('canonical:help/terms');
+        } else {
+            $info['tosUrl'] = '';
+        }
+
+        return $info;
+    }
+}
